5fe44111cc1c97e4349142197eb052c6c91c3acd
Rafikk98 <rafikk1998@gmail.com>
2023-04-29 19:39:39 +0600

    Modifed how sounds should play

--- DOM-Drum-Kit/index.js
+++ DOM-Drum-Kit/index.js
@@ -1,42 +1,58 @@
-let drumButtons = document.querySelectorAll('.drum');
-let numberOfDrumButtons = drumButtons.length;
+const drumButtons = document.querySelectorAll('.drum');
+const numberOfDrumButtons = drumButtons.length;
 
-let tom1 = new Audio('sounds/tom-1.mp3');
-let tom2 = new Audio('sounds/tom-2.mp3');
-let tom3 = new Audio('sounds/tom-3.mp3');
-let tom4 = new Audio('sounds/tom-4.mp3');
-let snare = new Audio('sounds/snare.mp3');
-let crash = new Audio('sounds/crash.mp3');
-let kick = new Audio('sounds/kick.mp3');
+const tom1 = new Audio('sounds/tom-1.mp3');
+const tom2 = new Audio('sounds/tom-2.mp3');
+const tom3 = new Audio('sounds/tom-3.mp3');
+const tom4 = new Audio('sounds/tom-4.mp3');
+const snare = new Audio('sounds/snare.mp3');
+const crash = new Audio('sounds/crash.mp3');
+const kickBass = new Audio('sounds/kick-bass.mp3');
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
     drumButtons[i].addEventListener('click', function () {
         let buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML) {
-            case 'w':
-                tom1.play();
-                break;
-            case 'a':
-                tom2.play();
-                break;
-            case 's':
-                tom3.play();
-                break;     
-            case 'd':
-                tom4.play();
-                break;
-            case 'j':
-                snare.play();
-                break;
-            case 'k':
-                crash.play();
-                break;
-            case 'l':
-                kick.play();
-                break;
-            default:
-                console.log(buttonInnerHTML);
-                break;
-        }
+        makeSound(buttonInnerHTML);
     });
+}
+
+document.addEventListener('keypress', (event) => {
+    makeSound(event.key);
+});
+
+
+function makeSound(key) {
+    switch (key) {
+        case 'w':
+            tom1.currentTime = 0;
+            tom1.play();
+            break;
+        case 'a':
+            tom2.currentTime = 0;
+            tom2.play();
+            break;
+        case 's':
+            tom3.currentTime = 0;
+            tom3.play();
+            break;     
+        case 'd':
+            tom4.currentTime = 0;
+            tom4.play();
+            break;
+        case 'j':
+            snare.currentTime = 0;
+            snare.play();
+            break;
+        case 'k':
+            crash.currentTime = 0;
+            crash.play();
+            break;
+        case 'l':
+            kickBass.currentTime = 0;
+            kickBass.play();
+            break;
+        default:
+            console.log(key);
+            break;
+    }
 }
