f1a093632821d020554e92c9fac2ec66ef043c34
Rafikk98 <rafikk1998@gmail.com>
2023-05-14 14:33:10 +0600

    ejs-challenge done

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,39 +1,54 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 const port = 3000;
 
 const app = express();
 
 const items = ['Buy Food', 'Cook Food', 'Eat Food'];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    let today = new Date();
-
-    let options = {
-        weekday : 'long',
-        day: 'numeric',
-        month: 'long'
-    };
-
-    let day = today.toLocaleDateString('en-US', options);
-
+    const day = date.getDate();
     res.render('list', {
-        dayName : day,
+        listTitle : day,
         newListItems : items
     });
 });
 
 app.post('/', (req, res) => {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
+    if(req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect('/work');
+    } else {
+        items.push(item);
+        res.redirect('/');
+    }
+});
 
-    items.push(item);
-    res.redirect('/');
-})
 
+app.get('/work', (req, res) => {
+    res.render('list', {
+        listTitle: "Work", 
+        newListItems: workItems
+    });
+});
+
+// app.post('/work', (req, res) => {
+//     const item = req.body.newItem;
+//     if(req.body.list === "Work") {
+//         workItems.push(item);
+//         res.redirect('/work');
+//     } else {
+//         items.push(item);
+//         res.redirect('/');
+//     }
+// });
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
